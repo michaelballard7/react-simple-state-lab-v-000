87b9ff90d11a10dccbb38937305d52aea9de4a64
Michael Ballard <ballard.michael9@gmail.com>
2020-08-16 14:03:58 +0000

    Automatically backed up by Learn

---
+++ src/Cell.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+class Cell extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    this.state = {
+        color : this.props.value
+    }
+}
